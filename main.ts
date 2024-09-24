b9562927c6b6c114bcd8a27384b8b7631eedfe66
Nico-Sellitto <nico.sellitto@mths.ca>
2024-09-24 10:52:13 -0400

    made it show temperature in kelvin

--- main.ts
+++ main.ts
@@ -2,16 +2,15 @@
  *
  * Created by: Nico Sellitto
  * Created on: Sep 2024
- * This program shows temperature
+ * This program shows temperature in kelvin
 */
 
 basic.showIcon(IconNames.Happy)
 
-let temp = input.temperature()
+let temp = input.temperature() + 273.15
 
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
-    basic.showString('the temperature is:')
-    basic.showNumber(temp)
+    basic.showString('the temperature is:' + Math.round(temp) + " k")
     basic.showIcon(IconNames.Happy)
 })
